73a61ab031efd85de78f4d7324c20242e40b7c51
Philipp A <flying-sheep@web.de>
2016-05-20 11:09:00 +0200

    added some vector tests

---
+++ tests/testthat/test_vector.r
@@ -0,0 +1,31 @@
+context('Vectors')
+
+test_that('1 element vectors display correctly', {
+	expect_identical(repr_html(1), '1')
+})
+
+test_that('plain vectors display correctly', {
+	expect_identical(repr_html(c(1, 2)), '<ol class=list-inline>
+\t<li>1</li>
+\t<li>2</li>
+</ol>
+')
+})
+
+test_that('named vectors display correctly', {
+	expect_identical(repr_html(c(a = 1, b = 2)), '<dl class=dl-horizontal>
+\t<dt>a</dt>
+\t\t<dd>1</dd>
+\t<dt>b</dt>
+\t\t<dd>2</dd>
+</dl>
+')
+})
+
+test_that('factors display correctly', {
+	expect_identical(repr_html(factor(c('a', 'b'))), '<ol class=list-inline>
+\t<li>a</li>
+\t<li>b</li>
+</ol>
+')
+})
