37903e15925a776b4cf5a788e1050001fcbc0555
Carson Sievert <cpsievert1@gmail.com>
2017-02-01 16:40:43 -0600

    naive implementation of HTML tag rendering (for htmltools)

--- R/repr_htmlwidget.r
+++ R/repr_htmlwidget.r
@@ -22,3 +22,57 @@ repr_html.htmlwidget <- function(obj, ...) {
 	
 	readChar(htmlfile, file.info(htmlfile)$size)
 }
+
+
+
+# sort of like htmlwidgets::saveWidget(), but for HTML tags
+save_html <- function(obj, ...) {
+	
+	if (!requireNamespace('htmlwidgets')) {
+		stop('Please install the htmlwidgets package')
+	}
+	if (!getFromNamespace("pandoc_available", asNamespace("htmlwidgets"))()) {
+		stop("Printing HTML tags requires pandoc. For details see:\n", 
+				 "https://github.com/rstudio/rmarkdown/blob/master/PANDOC.md")
+	}
+	
+	htmlfile <- tempfile(fileext = '.html')
+	libdir <- file.path(dirname(htmlfile), "lib")
+	htmltools::save_html(obj, file = htmlfile, libdir = libdir)
+	
+	# TODO: it would be nice if we didn't have to rely on pandoc 
+	# https://github.com/rstudio/htmltools/issues/73
+	getFromNamespace("pandoc_self_contained_html", asNamespace("htmlwidgets"))(htmlfile, htmlfile)
+	
+	readChar(htmlfile, file.info(htmlfile)$size)
+}
+
+#' HTML tag lists
+#'
+#' Standalone HTML representation and dummy text representation
+#'
+#' @param obj  The tag list to create a representation for
+#' @param ...  ignored
+#'
+#' @name repr_*.shiny.tag.list
+#' @export
+repr_text.shiny.tag.list <- function(obj, ...) 'HTML tag lists cannot be represented in plain text (need html)'
+
+#' HTML tags
+#'
+#' Standalone HTML representation and dummy text representation
+#'
+#' @param obj  The tag list to create a representation for
+#' @param ...  ignored
+#'
+#' @name repr_*.shiny.tag
+#' @export
+repr_text.shiny.tag <- function(obj, ...) 'HTML tags cannot be represented in plain text (need html)'
+	
+#' @name repr_*.shiny.tag.list
+#' @export
+repr_html.shiny.tag.list <- save_html
+
+#' @name repr_*.shiny.tag
+#' @export
+repr_html.shiny.tag <- save_html
