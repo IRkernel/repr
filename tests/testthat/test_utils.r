ab4f361d076985d01765981e45452e604b6fa799
Philipp A <flying-sheep@web.de>
2018-11-07 15:05:43 +0100

    v0.17: Make tests run on windows

--- tests/testthat/test_utils.r
+++ tests/testthat/test_utils.r
@@ -12,27 +12,24 @@ test_that('data_uris works with one or multiple files', {
 	f1 <- tempfile(fileext = '.js')
 	f2 <- tempfile(fileext = '.js')
 
-	fh <- file(f1)
-	writeLines('one', fh)
+	fh <- file(f1, 'wb')
+	writeBin(charToRaw('one'), fh)
 	close(fh)
 
-	fh <- file(f2)
-	writeLines('two', fh)
+	fh <- file(f2, 'wb')
+	writeBin(charToRaw('two'), fh)
   close(fh)
 
 	files <- c(f1, f2)
 
 	expect_equal(
-		as.character(data_uris(mime = 'application/javascript', files = files[[1L]])),
-		'data:application/javascript;base64,b25lCg=='
+		as.character(data_uris(mime = 'text/plain', files = files[[1L]])),
+		'data:text/plain;base64,b25l'
 	)
 
 	expect_equal(
-		as.character(data_uris(mime = 'application/javascript', files = files)),
-		c(
-			'data:application/javascript;base64,b25lCg==',
-			'data:application/javascript;base64,dHdvCg=='
-		)
+		as.character(data_uris(mime = 'text/plain', files = files)),
+		paste0('data:text/plain;base64,', c('b25l', 'dHdv'))
 	)
 
 	file.remove(f1)
