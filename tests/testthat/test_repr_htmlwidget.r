e576665c13fbe0680d89a0a82c3e347bae6309d5
Philipp A <flying-sheep@web.de>
2018-12-25 21:29:02 +0100

    Fixed htmlwidgets test

--- tests/testthat/test_repr_htmlwidget.r
+++ tests/testthat/test_repr_htmlwidget.r
@@ -18,7 +18,7 @@ test_that('Dependencies work', {
 	skip_if_not_installed('htmlwidgets')
 	
 	r <- repr_html(stringr::str_view('xy', 'y'))
-	expect_match(r, '<script src="data:application/javascript', fixed = TRUE, all = FALSE)
+	expect_match(r, '<script title="htmlwidgets" src="data:application/javascript', fixed = TRUE, all = FALSE)
 })
 
 test_that('The dependency manager works', {
