7baa0ae3c871052ba9fc196491cae4050292c9e4
Philipp A <flying-sheep@web.de>
2020-08-22 15:51:39 +0200

    Add description

--- R/package.r
+++ R/package.r
@@ -1,6 +1,7 @@
 #' The repr package
 #' 
-#' @details
+#' String and binary representations of objects for several formats / mime types.
+#' 
 #' The LaTeX repr of vectors needs \code{\\usepackage[inline]{enumitem}}
 #' 
 #' The LaTeX repr of functions with the \code{repr.function.highlight} option set to FALSE needs \code{\\usepackage{minted}}
